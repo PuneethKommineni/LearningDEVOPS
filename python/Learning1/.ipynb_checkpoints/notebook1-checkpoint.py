fa2fc4c206415d250d57ee6d53af3fe9fc6a0d50
Puneeth <puneethkommineni95@gmail.com>
2021-12-20 22:26:43 +0530

    Create notebook1-checkpoint.py

---
+++ python/Learning1/.ipynb_checkpoints/notebook1-checkpoint.py
@@ -0,0 +1,48 @@
+      "\u001b[1;31mTypeError\u001b[0m: can only concatenate str (not \"int\") to str"
+     ]
+    }
+   ],
+   "source": [
+    "a=222\n",
+    "a=\"puneeth\"\n",
+    "b=\"222\"\n",
+    "c=100\n",
+    "d=b+c\n",
+    "print(a)\n",
+    "print(\"a\")\n",
+    "print('a')\n",
+    "print(b)\n",
+    "print(d)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "2390a23d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
